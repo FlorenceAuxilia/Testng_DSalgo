8110f43c1af713ff78572d6939fffd2fe7bd71c3
Florence Auxilia Emmanuel <122188465+FlorenceAuxilia@users.noreply.github.com>
2024-11-26 22:41:08 -0700

    Update TC008_LoginValidation.java

--- src/test/java/testcases/TC008_LoginValidation.java
+++ src/test/java/testcases/TC008_LoginValidation.java
@@ -31,63 +31,63 @@ public class TC008_LoginValidation extends BaseClass{
 		    loginpage_obj =new DSAlgoLoginPom(driver.get());
 	}
 	
-//	@Test(priority = 1 ,groups={"Login","FunctionalTesting"})
-//	public void passwordError() throws Throwable {
-//	//	getDriver().get(p.getProperty("loginUrl"));
-//		loginpage_obj.enterUsername(p.getProperty("username"));
-//		loginpage_obj.Clciklogin();
-//		
-//		if(loginpage_obj.checkForErrorpopup().equalsIgnoreCase(p.getProperty("LoginErrorPopup")))
-//		 {
-//			 logger.info("Test passed");
-//		    	Assert.assertTrue(true);
-//		 }
-//		 else
-//		 {
-//			 logger.error("Test failed..");
-//			logger.debug("Debug logs...");
-//			 Assert.assertTrue(false);
-//		 }
-//		
-//	}
+	@Test(priority = 1 ,groups={"Login","FunctionalTesting"})
+	public void passwordError() throws Throwable {
+	//	getDriver().get(p.getProperty("loginUrl"));
+		loginpage_obj.enterUsername(p.getProperty("username"));
+		loginpage_obj.Clciklogin();
+		
+		if(loginpage_obj.checkForErrorpopup().equalsIgnoreCase(p.getProperty("LoginErrorPopup")))
+		 {
+			 logger.info("Test passed");
+	    	Assert.assertTrue(true);
+		 }
+		 else
+		 {
+			 logger.error("Test failed..");
+			logger.debug("Debug logs...");
+			 Assert.assertTrue(false);
+		 }
+		
+	}
 	
 	
-//	@Test(priority = 2 ,groups={"Login","FunctionalTesting"})
-//	public void usernameError() throws Throwable {
-//		getDriver().get(p.getProperty("loginUrl"));
-//		loginpage_obj.enterPassword("username");
-//		loginpage_obj.Clciklogin();
-//		if(loginpage_obj.checkForErrorpopup().equalsIgnoreCase(p.getProperty("LoginErrorPopup")))
-//		 {
-//			 logger.info("Test passed");
-//		    	Assert.assertTrue(true);
-//		 }
-//		 else
-//		 {
-//			 logger.error("Test failed..");
-//			logger.debug("Debug logs...");
-//			 Assert.assertTrue(false);
-//		 }
-//		
-//	}
-//	
-//	@Test(priority = 3 ,groups={"Login","FunctionalTesting"})
-//	public void Registrationpage()
-//	{
-//		getDriver().get(p.getProperty("loginUrl"));
-//		loginpage_obj.clickRegister();
-//		if(loginpage_obj.getPageTitle().equals(p.getProperty("RegistrationPageTitle")))
-//		 {
-//			 logger.info("Test passed");
-//			 Assert.assertTrue(true);
-//		 }
-//		 else
-//		 {
-//			 logger.error("Test failed..");
-//			 logger.debug("Debug logs...");
-//			 Assert.assertTrue(false);
-//		 }
-//		}
+	@Test(priority = 2 ,groups={"Login","FunctionalTesting"})
+	public void usernameError() throws Throwable {
+		getDriver().get(p.getProperty("loginUrl"));
+		loginpage_obj.enterPassword("username");
+		loginpage_obj.Clciklogin();
+		if(loginpage_obj.checkForErrorpopup().equalsIgnoreCase(p.getProperty("LoginErrorPopup")))
+		 {
+			 logger.info("Test passed");
+		    	Assert.assertTrue(true);
+		 }
+		 else
+		 {
+			 logger.error("Test failed..");
+			logger.debug("Debug logs...");
+			 Assert.assertTrue(false);
+		 }
+		
+	}
+	
+	@Test(priority = 3 ,groups={"Login","FunctionalTesting"})
+	public void Registrationpage()
+	{
+		getDriver().get(p.getProperty("loginUrl"));
+		loginpage_obj.clickRegister();
+		if(loginpage_obj.getPageTitle().equals(p.getProperty("RegistrationPageTitle")))
+		 {
+			 logger.info("Test passed");
+			 Assert.assertTrue(true);
+		 }
+		 else
+		 {
+			 logger.error("Test failed..");
+			 logger.debug("Debug logs...");
+			 Assert.assertTrue(false);
+		 }
+		}
 	
 	@Test(priority=4,dataProvider="loginCredentials",dataProviderClass=DataProviders.class,groups={"Login","FunctionalTesting","DataDriven"})
 	public void runLogininvalid( String username,String password)  {
@@ -108,27 +108,27 @@ public class TC008_LoginValidation extends BaseClass{
 		 }
 
 	}
-//	
-//	@Test(priority = 6 ,groups={"Login","FunctionalTesting"})
-//	public void runLogin() throws Throwable {
-//		getDriver().get(p.getProperty("loginUrl"));
-//		loginpage_obj.enterUsername(p.getProperty("username"));
-//		loginpage_obj.enterPassword(p.getProperty("password"));
-//		loginpage_obj.Clciklogin();
-//		System.out.println(loginpage_obj.getPageTitle());
-//		if(loginpage_obj.getPageTitle().equals(p.getProperty("GettingStartedPageTitle")))
-//		 {
-//			 logger.info("Test passed");
-//			 Assert.assertTrue(true);
-//		 }
-//		 else
-//		 {
-//			logger.error("Test failed..");
-//			logger.debug("Debug logs...");
-//			 Assert.assertTrue(false);
-//		 }
-//
-//	}
-//	
+	
+	@Test(priority = 6 ,groups={"Login","FunctionalTesting"})
+	public void runLogin() throws Throwable {
+		getDriver().get(p.getProperty("loginUrl"));
+		loginpage_obj.enterUsername(p.getProperty("username"));
+		loginpage_obj.enterPassword(p.getProperty("password"));
+		loginpage_obj.Clciklogin();
+		System.out.println(loginpage_obj.getPageTitle());
+		if(loginpage_obj.getPageTitle().equals(p.getProperty("GettingStartedPageTitle")))
+		 {
+			 logger.info("Test passed");
+			 Assert.assertTrue(true);
+		 }
+		 else
+		 {
+			logger.error("Test failed..");
+			logger.debug("Debug logs...");
+			 Assert.assertTrue(false);
+		 }
+
+	}
+	
 
 }
