8fe5caa244c3a8e05c5fb481810ca66dfd616668
Florence Auxilia Emmanuel <122188465+FlorenceAuxilia@users.noreply.github.com>
2024-11-27 11:01:14 -0700

    Update BaseClass.java

--- src/test/java/testbase/BaseClass.java
+++ src/test/java/testbase/BaseClass.java
@@ -53,11 +53,11 @@ public DSAlgoTreePom treepage_obj;
 public DSAlgoGraphPom graphpage_obj;
 public DSAlgoDataStructure_IntroductionPom datastructurepage_obj;
 public DSAlgoArrayPom arraypage_obj;
-//public DSAlgoRegisterPom registerpage_obj;
+public DSAlgoRegisterPom registerpage_obj;
 public ExtentReports extent;
 
 
-	@BeforeMethod(groups= {"Stack","GetStarted","DataDriven","datastructure","FunctionalTesting","successful_login","Unsuccessful_login","Array"})
+	@BeforeMethod(groups= {"Stack","GetStarted","Graph","Tree","Login","Queue","DataDriven","datastructure","FunctionalTesting","successful_login","Unsuccessful_login","Array"})
 	@Parameters({"browser"})
 	public void setup(String br) throws IOException
 	{
@@ -88,7 +88,7 @@ public ExtentReports extent;
 	}
 	
 
-	@AfterMethod(groups= {"Stack","GetStarted","DataDriven","datastructure","FunctionalTesting","successful_login","Unsuccessful_login","Array"})
+	@AfterMethod(groups= {"Stack","GetStarted","Graph","Tree","Login","Queue","DataDriven","datastructure","FunctionalTesting","successful_login","Unsuccessful_login","Array"})
 	public void tearDown()
 	{
 		getDriver().quit();
